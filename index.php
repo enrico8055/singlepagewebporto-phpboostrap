7c0e669adbc11db5b4b351234385cd7aa75017c2
Enrico A <enrico.aurelius@gmail.com>
2023-07-05 11:19:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Material Design for Bootstrap</title>
+    <!-- MDB icon -->
+    <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon" />
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+    <!-- Google Fonts Roboto -->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Sofia:wght@300;400;500;700;900&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Lobster:wght@300;400;500;700;900&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;700;900&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Bangers:wght@300;400;500;700;900&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Titan One:wght@300;400;500;700;900&display=swap"
+    />
+    <!-- MDB -->
+    <link rel="stylesheet" href="css/mdb.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+    />
+
+    <title>EnCorp</title>
+  </head>
+  <body id="home">
+  
+    <nav class="navbar navbar-expand navbar-dark bg-dark shadow-sm p-2 sticky-top"  style="margin-bottom: 0vw;" data-aos="fade-down"
+     data-aos-anchor-placement="center-bottom" data-aos-duration="3000">
+        <div class="container text-center">
+            <div class="mx-auto">
+                <div class="row">
+                    <div class="col text-center mx-auto px-1">
+                        <a class="navbar-brand " href="#home" style="font-size: 25px">
+                            <i class="fab fa-qq" width="30" height="29" class="d-inline-block mb-1 ms-3"  data-mdb-toggle="animation" data-mdb-animation-reset="true" data-mdb-animation="slide-out-right"></i>
+                        </a>
+                    </div>
+
+                    <div class="col text-center mx-auto px-1">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" aria-current="page" href="#home" style="font-family: Mulish">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#achivement" style="font-family: Mulish">Achivement</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#contact" style="font-family: Mulish">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </nav>
+
+    <div class="container-fluid pt-1 " style="background-color:#F8F8F8;">
+        <div class="container pt-1" style=" padding-bottom:100px">
+            <div class="row">
+                <div class="col-md  my-auto" data-aos="fade-up-left" data-aos-duration="2000">
+                    <div class="mx-auto my-auto">
+                        <img src="img/main.png" class="img-fluid animate__animated animate__shakeY animate__infinite" alt="..." width="600" height="600" style="animation-duration: 30s;">
+                    </div>
+                </div>
+                <div class="col-md  my-auto" data-aos="fade-left" data-aos-duration="2000">
+                <div class=" text-center">
+                        <h1 class="text-center img-fluid" style="font-size: 2.9vw;font-family: Sofia">Hi, </h1> <h1 style="font-size: 3.5vw;font-family: Titan One;color:#4ADEDE;"> Quantum IT </h1> <h1 class="text-center img-fluid" style="font-size: 2.9vw;font-family: Sofia"> Will Solve Your Problem!</h1>
+                        <div class="img-fluid animate__animated animate__rubberBand" style="animation-delay: 2s;">
+                        <a href="#started" class="btn-sm btn-primary rounded-pill " id="start" style="text-decoration:none; background-color:#4ADEDE">Get Started!</a>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" id="started"><path fill="#F8F8F8" fill-opacity="1" d="M0,224L48,213.3C96,203,192,181,288,192C384,203,480,245,576,218.7C672,192,768,96,864,96C960,96,1056,192,1152,213.3C1248,235,1344,181,1392,154.7L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+
+    <div class="container-fluid mb-5" style="margin-top:50px;" >
+        <div class="mx-auto " style="width:fit-content">
+        <div class="row" >
+            <div class="col-1 text-center " style="width:fit-content">
+                <div class="" style="height:8vw;width:1vw;background-color:#4ADEDE" data-aos="flip-right" data-aos-duration="2000" ></div>
+            </div>
+            <div class="col-5  text-center " style="width:fit-content" >
+                <h1 style="font-size:7vw;font-family: Lobster" data-aos="fade-right" data-aos-duration="3000" >Who Are We? </h1>
+            </div>
+        </div>
+        </div>
+        <div class="row mt-5" >
+            <div class="col  text-center">
+                <div class="card text-dark mb-3 shadow " style="background-color:#cfcfcf; max-height:350px" data-aos="fade-right" data-aos-duration="1500">
+                    <div class="card-header">
+                        <i class="fas fa-thumbtack" width="30" height="29" class="d-inline-block mb-1 ms-3"  data-mdb-toggle="animation" data-mdb-animation-reset="true" data-mdb-animation="slide-out-right"></i>
+                        <b style="font-family: Banngers">History</b>
+                    </div>
+                    <div class="card-body">
+                        <p style="font-size:1.5vw">Quantum it is a subsidiary of quantum group which was founded in 2021 by Enrico. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col  text-center" >
+                <div class="card text-dark mb-3 shadow" style="background-color:#e3e3e3; max-height:350px" data-aos="fade-right" data-aos-duration="2000">
+                    <div class="card-header">
+                        <i class="fas fa-thumbtack" width="30" height="29" class="d-inline-block mb-1 ms-3"  data-mdb-toggle="animation" data-mdb-animation-reset="true" data-mdb-animation="slide-out-right"></i>
+                        <b style="font-family: Banngers">Vision</b></div>
+                    <div class="card-body">
+                        <p style="font-size:1.5vw">Quantum it has a mission to help all the IT problems you have and digitize all your business to make it more efficient and sophisticated.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col  text-center" >
+                <div class="card text-dark mb-3 shadow" style="background-color:#f5f5f5; max-height:350px" data-aos="fade-right" data-aos-duration="2500">
+                    <div class="card-header">
+                        <i class="fas fa-thumbtack" width="30" height="29" class="d-inline-block mb-1 ms-3"  data-mdb-toggle="animation" data-mdb-animation-reset="true" data-mdb-animation="slide-out-right"></i>
+                        <b style="font-family: Banngers">Offer</b></div>
+                    <div class="card-body">
+                        <p style="font-size:1.5vw">What we offer are services in the form of web development, android applications, OS repairs, and other IT problems or needs, for more information please contact our customer service. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <svg style=" margin-top:30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#F8F8F8" fill-opacity="1" d="M0,96L48,122.7C96,149,192,203,288,197.3C384,192,480,128,576,138.7C672,149,768,235,864,261.3C960,288,1056,256,1152,224C1248,192,1344,160,1392,144L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#F8F8F8" fill-opacity="1" d="M0,224L48,213.3C96,203,192,181,288,192C384,203,480,245,576,218.7C672,192,768,96,864,96C960,96,1056,192,1152,213.3C1248,235,1344,181,1392,154.7L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+
+    <div class="" style="margin-top:50px; margin-bottom:100px" data-aos="fade-down"
+     data-aos-anchor-placement="center-bottom" data-aos-duration="3000">
+        <div class="col-mb  text-center pt-5" id="achivement" >
+                    <h1 style="font-size:9vw;font-family: Lobster" data-aos="zoom-in-right" data-aos-duration="3000"> Achivement </h1>
+        </div>
+
+        <div id="carouselExampleCaptions" class="carousel slide mt-5 p-2" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active  ">
+            <img src="img/1.jpg" class="d-block w-70 h-70  mx-auto img-fluid rounded-pill" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="img/2.jpg" class="d-block w-70 h-70 mx-auto img-fluid rounded-pill" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        </div>
+
+        
+        </div>
+        <div class="container-fluid" id="contact" style="margin-top:200px; height:fit-content;background-color:#F8F8F8;padding-top:25px;padding-bottom:0px" data-aos="fade-up" data-aos-duration="2000">
+            <div class="row">
+                <div class="col-md">
+                    <div class="row">
+                        <img src="img/last.png" class="mx-auto img-fluid img-thumbnail rounded-circle" alt="..." style="height:200px;width:200px">
+                    </div>
+                    <div class="row text-center" style="font-family: Mulish"><h3>Contact Us</div>
+                </div>
+                <div class="col-md mt-4">
+                    <dl class="row">
+                        <dt class="col-sm-3">Address</dt>
+                        <dd class="col-sm-9">Jalan Puri Anjasmoro K4, Semarang</dd>
+
+                        <dt class="col-sm-3">Phone</dt>
+                        <dd class="col-sm-9">
+                            <p>(+62) 81224670606</p>
+                        </dd>
+
+                        <dt class="col-sm-3">WhatsApp</dt>
+                        <dd class="col-sm-9">(+62) 81224670606</dd>
+
+                        <dt class="col-sm-3 text-truncate">Operational Hour</dt>
+                        <dd class="col-sm-9">24/7</dd>
+
+                        <dt class="col-sm-3">Term And Condition</dt>
+                        <dd class="col-sm-9">
+                            <dl class="row">
+                            <dd class="col-sm-8">After the order is paid the buyer cannot change the order.</dd>
+                            </dl>
+                        </dd>
+                    </dl>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+    <!-- MDB -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Custom scripts -->
+    <script type="text/javascript"></script>
+  </body>
+</html>
